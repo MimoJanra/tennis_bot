cd4c3c6a31de8b4613303c220af6e6ab9186572c
Artem Alekseev <alekseev@pho.to>
2024-06-10 18:38:21 +0300

    first

---
+++ src/main/java/org/telegram/Main.java
@@ -0,0 +1,16 @@
+package org.telegram;
+
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        String botToken = "12345:YOUR_TOKEN";
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new Keyboard(botToken));
+            System.out.println("BotApi20 successfully started!");
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
